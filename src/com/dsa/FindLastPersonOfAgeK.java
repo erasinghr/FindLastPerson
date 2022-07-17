7d4cb46989df1b31b7dd71c2e30dbf63c98f2e65
Abhinav Singh <57244254+erasinghr@users.noreply.github.com>
2022-07-17 12:07:43 +0530

    Update FindLastPersonOfAgeK.java

--- src/com/dsa/FindLastPersonOfAgeK.java
+++ src/com/dsa/FindLastPersonOfAgeK.java
@@ -1,2 +1,47 @@
-package com.dsa;public class FindLastPersonOfAgeK {
+package com.dsa;
+public class FindLastPersonOfAgeK {
+  public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        List<Integer> ages = new ArrayList<Integer>();
+        List<String> people = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            ages.add(sc.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            people.add(sc.next());
+        }
+        String ans = findLastPersonOfAgeK(ages, people, k);
+        System.out.println(ans);
+        sc.close();
+    }
+
+    static String findLastPersonOfAgeK(List<Integer> ages, List<String> people, int k){
+        LinkedHashMap<String, Integer> personAgeMap = new LinkedHashMap<>();
+        List<String> personNameList = new ArrayList<>();
+        for(int i =0; i<people.size(); i++){
+            personAgeMap.put(people.get(i), ages.get(i));
+        }
+        int count = 0;
+        for(int age: ages){
+            if(age == k){
+                count += 1;
+            }
+        }
+        if(count == 0){
+            return ("Not found");
+        }
+        for (Map.Entry<String, Integer>
+                entry : personAgeMap.entrySet()){
+            if(count == 1 && entry.getValue() == k){
+                return entry.getKey();
+
+            }
+            else if(count>1 && entry.getValue() == k){
+                personNameList.add(entry.getKey());
+            }
+        }
+        return personNameList.get(personNameList.size()-1);
+    }
 }
